e318d34912193ba47583e2b529c237930d40dff0
Kay <khurrammohammed1990@gmail.com>
2019-10-11 12:55:32 +0100

    updated with adding additional modules

--- tutorial.js
+++ tutorial.js
@@ -1,2 +1,10 @@
 const sum = (num1, num2) => { return num1 + num2 };
-module.exports = sum;
+const PI = 3.14;
+class SomeObject {
+    constructor() {
+        console.log("Object created");
+    }
+}
+module.exports.sum = sum;
+module.exports.PI = PI;
+module.exports.SomeObject = SomeObject;
