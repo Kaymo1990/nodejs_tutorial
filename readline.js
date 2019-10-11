a3f6b6c95fe9aace187e6dca101924dc7b30d594
Kay <khurrammohammed1990@gmail.com>
2019-10-11 23:45:03 +0100

    integrated close listener callback

--- readline.js
+++ readline.js
@@ -7,5 +7,16 @@ let n2 = Math.floor((Math.random() * 10) + 1);
 let ans = n1 + n2;
 
 rl.question(`What is ${n1} + ${n2}?`, (userInput)=> {
-    console.log(userInput);
+    if (userInput == ans) {
+        console.log("That is correct!");
+    }
+    else{
+        console.log("You are dumb.");
+    }
+
+    rl.close();
+});
+
+rl.on('close', ()=> {
+    console.log('See you next time!');
 });
