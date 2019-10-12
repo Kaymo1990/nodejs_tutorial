aa6a217eb2ff0e31278b4574884092ea68b1f770
Kay <khurrammohammed1990@gmail.com>
2019-10-12 01:59:55 +0100

    files renamed

--- filesystem.js
+++ filesystem.js
@@ -1,6 +1,21 @@
 const fs = require('fs');
-fs.writeFile('example.txt', "this is an example", (error) => {if(error)
-{console.log(error);}
-else{
-console.log('file created');
-}})
+// fs.writeFile('example.txt', "this is an example", (err) => {if(err)
+// // {console.log(err);}
+// // else{
+// // console.log('file created');
+// // fs.readFile('example.txt', 'utf8', (err, file) => {
+// // if (err) {
+// //     console.log(err);
+// // }
+// // else {
+// //     console.log(file);
+// // }
+// // })
+// // }})
+
+fs.rename('example.txt', 'newfile.txt', (err) => {if (err) {
+    console.log(err);}
+else {
+console.log('renamed!');
+}
+})
