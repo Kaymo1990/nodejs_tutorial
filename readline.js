7bb068cc6b7cbd013bb2d11d257711087d2e4648
Kay <khurrammohammed1990@gmail.com>
2019-10-12 01:26:26 +0100

    added callback for incorrect answers

--- readline.js
+++ readline.js
@@ -9,12 +9,23 @@ let ans = n1 + n2;
 rl.question(`What is ${n1} + ${n2}?`, (userInput)=> {
     if (userInput == ans) {
         console.log("That is correct!");
+        rl.close();
     }
     else{
         console.log("You are dumb.");
-    }
+        rl.setPrompt('try again');
+        rl.prompt();
+        rl.on('line', (userInput)=> {
+            if (userInput == ans) {
+                rl.close();
+            }
+            else {
+                rl.setPrompt('try again');
+                rl.prompt();
+            }
 
-    rl.close();
+        })
+    }
 });
 
 rl.on('close', ()=> {
